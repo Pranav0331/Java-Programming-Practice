0119b779a11191437233262f9637522d6c00b824
pranavmathur36 <pranavmathur36@gmail.com>
2026-09-02 00:24:27 +0530

    Hash Start

---
+++ Hash Set/h1_Create.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class h1_Create {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer>set=new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+
+        set.remove(1);
+        System.out.println(set);
+
+        System.out.println(set.size());
+
+
+
+    }
+    
+}
